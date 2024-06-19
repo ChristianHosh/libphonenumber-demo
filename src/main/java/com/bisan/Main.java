00a5d0c927773aff301b1dbad5903a06bdaba934
Christian Hosh <104357056+ChristianHosh@users.noreply.github.com>
2024-06-19 18:11:03 +0300

    region code parsing

--- src/main/java/com/bisan/Main.java
+++ src/main/java/com/bisan/Main.java
@@ -519,6 +519,7 @@ public class Main {
   public static final PhoneNumberUtil PHONE_NUMBER_UTIL = PhoneNumberUtil.getInstance();
   public static final PhoneNumberOfflineGeocoder OFFLINE_GEOCODER = PhoneNumberOfflineGeocoder.getInstance();
 
+
   public static final String CMD_EXIT = "exit";
 
   public static void main(String[] args) {
@@ -527,25 +528,41 @@ public class Main {
     int failed = 0;
     int total = 0;
     int success = 0;
+    int valid = 0;
+    int invalid = 0;
     List<String> failedNumbers = new ArrayList<>();
+    List<String> invalidNumbers = new ArrayList<>();
+
     for (int i = 0; i < phoneNumbers.size(); i++) {
       String number = phoneNumbers.get(i);
       String[] asArgs = new String[]{number};
+      boolean isValid;
       try {
-        parse(asArgs, i);
+        isValid = parse(asArgs, i);
         success++;
       } catch (Exception e) {
-        e.printStackTrace();
+        e.printStackTrace(System.err);
         failedNumbers.add(number);
         failed++;
+        isValid = false;
       }
       total++;
+      if (isValid)
+        valid++;
+      else {
+        invalid++;
+        invalidNumbers.add(number);
+      }
+
     }
 
     System.out.println("total: " + total);
     System.out.println("success: " + success);
     System.out.println("failed: " + failed);
+    System.out.println("valid: " + valid);
+    System.out.println("invalid: " + invalid);
     System.out.println("failed numbers: " + failedNumbers);
+    System.out.println("invalid numbers: " + invalidNumbers);
   }
 
   private static void work() {
@@ -572,24 +589,26 @@ public class Main {
     }
   }
 
-  public static void parse(String[] args, int index) throws NumberParseException {
+  public static boolean parse(String[] args, int index) throws NumberParseException {
     String number = args[0];
-    String countryCode = args[args.length - 1];
 
-    if (countryCode.equals(number)) {
-      // no country code use 'IL' (System Default)
-      countryCode = "IL";
-    }
+    System.out.println("\n [" + index + "] NUMBER: " + number);
 
+    String countryCode = getCountryCode(number);
+    String regionCode = PHONE_NUMBER_UTIL.getRegionCodeForCountryCode(Integer.parseInt(countryCode));
+    System.out.println("REGION: " + regionCode);
+
+    boolean isValid;
     try {
-      Phonenumber.PhoneNumber phoneNumber = PHONE_NUMBER_UTIL.parse(number, countryCode);
-      System.out.println("\n [" + index + "] NUMBER: " + number);
+      Phonenumber.PhoneNumber phoneNumber = PHONE_NUMBER_UTIL.parse(number, regionCode);
+      isValid = PHONE_NUMBER_UTIL.isValidNumber(phoneNumber);
+
       System.out.println("country code:\t\t\t\t" + phoneNumber.getCountryCode());
       System.out.println("national number:\t\t\t" + phoneNumber.getNationalNumber());
       System.out.println("extension:\t\t\t\t" + phoneNumber.getExtension());
       System.out.println("region:\t\t\t\t\t\t" + PHONE_NUMBER_UTIL.getRegionCodeForNumber(phoneNumber));
       System.out.println("is possible number:\t\t\t" + PHONE_NUMBER_UTIL.isPossibleNumber(phoneNumber));
-      System.out.println("is valid number:\t\t\t" + PHONE_NUMBER_UTIL.isValidNumber(phoneNumber));
+      System.out.println("is valid number:\t\t\t" + isValid);
       System.out.println("number type:\t\t\t\t" + PHONE_NUMBER_UTIL.getNumberType(phoneNumber));
       System.out.println("region:\t\t\t\t\t\t" + OFFLINE_GEOCODER.getDescriptionForNumber(phoneNumber, Locale.ENGLISH));
       System.out.println("FORMATTING RESULTS");
@@ -597,10 +616,20 @@ public class Main {
       System.out.println("INTERNATIONAL:\t\t\t\t" + PHONE_NUMBER_UTIL.format(phoneNumber, PhoneNumberUtil.PhoneNumberFormat.INTERNATIONAL));
       System.out.println("NATIONAL:\t\t\t\t\t" + PHONE_NUMBER_UTIL.format(phoneNumber, PhoneNumberUtil.PhoneNumberFormat.NATIONAL));
       System.out.println("RFC3966:\t\t\t\t\t" + PHONE_NUMBER_UTIL.format(phoneNumber, PhoneNumberUtil.PhoneNumberFormat.RFC3966));
+
     } catch (NumberParseException e) {
       System.err.println("error parsing number: " + number);
       throw e;
     }
+    return isValid;
+  }
 
+  private static String getCountryCode(String number) {
+    int firstIndex = number.indexOf("(");
+    int lastIndex = number.indexOf(")");
+    if (firstIndex == -1 || lastIndex == -1) {
+      return number;
+    }
+    return number.substring(firstIndex + 1, lastIndex);
   }
 }
